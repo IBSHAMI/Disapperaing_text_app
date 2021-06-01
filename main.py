ca736462fa811c476b760b56e8c653c82438e78d
IBSHAMI <78549117+IBSHAMI@users.noreply.github.com>
2021-06-01 03:58:27 -0700

    edit color and design

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ import time
 class MainWindow(Tk):
     def __init__(self):
         Tk.__init__(self)
-        self.config(bg="#e3fdfd")
+        self.config(bg="#231e23")
         self.wm_geometry("800x500")
-        
-        self.title_label = Label(text="Type like your life depends on it!!!")
-        self.title_label.grid(row=1 ,column=2)
 
-        self.timer = Text(self, height=1, width=20, highlightthickness=2.8,
+        self.title_label = Label(text="Type like your life depends on it!!! \n Once you stop typing the timer will begin", bg="#231e23", font=("Colfax", 18, "italic"),
+                              fg="#39a6a3")
+        self.title_label.grid(row=0, column=1)
+
+        self.timer = Text(self, height=2, width=10, highlightthickness=2.8,
                           highlightbackground="#00adb5")
-        self.timer.grid(row=0, column=0, pady=10)
+        self.timer.grid(row=0, column=2, pady=10, padx=120)
 
 
 if __name__ == "__main__":
